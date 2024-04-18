3d7739cea9ffde9edfb957aa33810679ac3d4df9
Lucas Mezencio <lucasmezencio@outlook.com>
2024-04-18 17:29:10 -0300

    feat: aux functions

--- ArvoreAVL.c
+++ ArvoreAVL.c
@@ -9,3 +9,20 @@ struct NO {
     struct NO *esq;
     struct NO *dir;
 };
+
+int alt_NO(struct NO *no) {
+    if (no == NULL)
+        return -1;
+    return no->alt;
+}
+
+int fator_balanceamento_NO(struct NO *no) {
+    return labs(alt_NO(no->esq) - alt_NO(no->dir));
+}
+
+int maior(int x, int y) {
+    if (x > y) {
+        return x;
+    }
+    return y;
+}
